d6b3834133f80d4b9b1384b66f24532f122c3fd5
Szr80 <sezerytbe80@gmail.com>
2026-01-14 17:31:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,29 @@
-// Service Worker (Arka Plan Bildirim Bekçisi)
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js');
+
+const firebaseConfig = {
+    databaseURL: "https://ana-uygulama-f22cb-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// Arka planda dinleme başlat
+db.ref('ortakKanal').on('value', (snapshot) => {
+    const data = snapshot.val();
+    if (data && data.metin) {
+        // Bildirimi göster
+        self.registration.showNotification('YAVRU UYGULAMA', {
+            body: data.metin,
+            icon: 'https://cdn-icons-png.flaticon.com/512/3103/3103611.png',
+            vibrate: [300, 100, 300],
+            tag: 'mesaj-bildirim',
+            renotify: true
+        });
+    }
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-// Bildirime tıklandığında uygulamayı aç
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then((windowClients) => {
-            if (windowClients.length > 0) {
-                return windowClients[0].focus();
-            }
-            return clients.openWindow('/');
-        })
-    );
+    event.waitUntil(clients.openWindow('/'));
 });
